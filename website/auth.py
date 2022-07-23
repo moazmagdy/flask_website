80cab3d8cf14f9273fbcf62d05bcef270964a433
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-07-23 16:08:04 +0200

    modify

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['Get', 'POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['Get', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+    
+        if len(email) < 4:
+            flash("Email must be greater than 4 characters!", category='error')
+        elif len(firstName) < 2:
+            flash("First Name must be greater than 2 characters!", category='error')
+        elif password1 != password2:
+            flash("Passwords must be the same!", category='error')
+        elif len(password1) < 7:
+            flash("Password length must be greater than 7 characters.", category='error')
+        else:
+            #Add user to database
+            flash("Account created!", category='success')
+    return render_template("sign_up.html")
