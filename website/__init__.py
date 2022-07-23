997fea9d966230c83944bb87e55bc54c9592eb24
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-07-23 19:50:22 +0200

    modify

--- website/__init__.py
+++ website/__init__.py
@@ -22,6 +22,14 @@ def create_app():
 
     create_database(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
 def create_database(app):
--- website/auth.py
+++ website/auth.py
@@ -22,7 +22,7 @@ def login():
                 flash('Incorrect password, try again.', category= 'error')
         else:
             flash("User does not exist!", category= 'error')
-    return render_template("login.html")
+    return render_template("login.html", user= current_user)
 
 @auth.route('/logout')
 @login_required
@@ -57,4 +57,4 @@ def sign_up():
             flash("Account created!", category='success')
             login_user(user, remember= True)
             return redirect(url_for('views.home'))
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user= current_user)
